fe08b027a4453bce2cce4c10d752662020d8b345
Eugene M <gaddeena@gmail.com>
2020-05-20 16:06:03 +0300

    Tested the DFS iterator.

---
+++ graphs/DFS_test.go
@@ -0,0 +1,46 @@
+package graphs
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestNode struct {
+	Node
+	Name string
+}
+
+func Test_DFS_1(T *testing.T) {
+	root := TestNode{
+		Name: "L0",
+	}
+
+	root.Add(&TestNode{
+		Name: "L10",
+	}).Add(&TestNode{
+		Name: "L20",
+	}).Add(&TestNode{
+		Name: "L30",
+	})
+
+	root.Add(&TestNode{
+		Name: "L11",
+	}).Add(&TestNode{
+		Name: "L21",
+	})
+
+	root.Add(&TestNode{
+		Name: "L12",
+	})
+
+	expected := "L0L10L20L30L11L21L12"
+	actual := ""
+
+	for node := range DFS(&root) {
+		actual = fmt.Sprintf("%s%s", actual, node.(*TestNode).Name)
+	}
+
+	assert.Equal(T, expected, actual)
+}
