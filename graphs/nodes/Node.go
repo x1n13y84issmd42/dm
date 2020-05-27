08681b89272fae6c80755b9fe78734fd50b99a96
Eugene M <gaddeena@gmail.com>
2020-05-27 11:00:23 +0300

    Decluttered the Node.

--- graphs/nodes/Node.go
+++ graphs/nodes/Node.go
@@ -9,38 +9,12 @@ type INode interface {
 	Adjacent() *Nodes
 
 	SetAdjacency(a IAdjacency)
-
-	// DEPRECATED
-	Add(n INode) INode
 }
 
 // Node is a directed graph node.
 type Node struct {
 	INode
 	A IAdjacency
-
-	// DEPRECATED
-	ChildNodes *Nodes
-}
-
-// init is DEPRECATED
-func (node *Node) init() {
-	if node.ChildNodes == nil {
-		node.ChildNodes = NewNodes()
-	}
-}
-
-// Add adds a child node. And it's DEPRECATED.
-func (node *Node) Add(n INode) INode {
-	node.init()
-	node.ChildNodes.Add(n)
-	return n
-}
-
-// NumChildren returns number of child nodes. And it's DEPRECATED.
-func (node *Node) NumChildren() int {
-	node.init()
-	return node.ChildNodes.Count()
 }
 
 // SetAdjacency sets the graph-specific adjacency implementation.
@@ -50,6 +24,5 @@ func (node *Node) SetAdjacency(a IAdjacency) {
 
 // Adjacent returns all the adjacent nodes.
 func (node *Node) Adjacent() *Nodes {
-	node.init()
 	return node.A.AdjacentNodes(node)
 }
