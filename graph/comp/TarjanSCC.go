b54f812cd229b619194405fcb593f8d394711fb0
Eugene M <gaddeena@gmail.com>
2020-07-08 17:34:14 +0300

    "Tested" TarjanSCC.

--- graph/comp/TarjanSCC.go
+++ graph/comp/TarjanSCC.go
@@ -3,4 +3,4 @@ package comp
 import "github.com/x1n13y84issmd42/gog/graph/contract"
 
 // TarjanSCC finds strongly connected components of a graph using the Robert Tarjan's algorithm.
-func TarjanSCC(root contract.Node) {}
+func TarjanSCC(graph contract.Graph) {}
